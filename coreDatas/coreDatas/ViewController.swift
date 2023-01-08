52d471e0765de92410eea0ecf2143b47416ed719
gdetosy <tosyjdm@gmail.com>
2023-01-08 14:01:43 +0300

    1

---
+++ coreDatas/coreDatas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDatas
+//
+//  Created by tosy on 8.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
