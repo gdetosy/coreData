76e4c59dfb58098a0ef69e4286c64335aadaa463
gdetosy <tosyjdm@gmail.com>
2023-01-08 21:30:53 +0300

    finish?

--- coreDatas/coreDatas/Controllers/ToDoViewController.swift
+++ coreDatas/coreDatas/Controllers/ToDoViewController.swift
@@ -5,67 +5,127 @@
 //  Created by tosy on 8.01.23.
 //
 
-import UIKit
 import CoreData
+import UIKit
 class ToDoViewController: UITableViewController {
-    var selectedCategory: CategoryModel?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var selectedCategory: CategoryModel? {
+        didSet {
+            self.title = selectedCategory?.name
+            getData()
+        }
     }
-
+    
+    var itemsArray = [ItemModel]()
+    
     @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        alert.addTextField { textField in textField.placeholder = "your task"
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        let add = UIAlertAction(title: "Add", style: .default) { [weak self] _
+            in
+                if let textField = alert.textFields?.first,
+                   let text = textField.text,
+                   text != "",
+                   let self = self
+                {
+                    let newItem = ItemModel(context: self.context)
+                    newItem.title = text
+                    newItem.done = false
+                    newItem.parentCategory = self.selectedCategory
+                
+                    self.itemsArray.append(newItem)
+                    self.saveItems()
+                    self.tableView.insertRows(at: [IndexPath(row: self.itemsArray.count - 1, section: 0)], with: .automatic)
+                }
+        }
+        alert.addAction(cancel)
+        alert.addAction(add)
+        self.present(alert, animated: true)
     }
-    
-    
     
     // MARK: - Table view data source
-
-  
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return itemsArray.count
     }
-
-  
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let item = itemsArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = itemsArray[indexPath.row].done ? .checkmark : .none
         return cell
     }
     
     // MARK: - Table view delegate
-    // Override to support conditional editing of the table view.
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     
         return true
     }
-  
 
-    
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            if editingStyle == .delete {
+                if let categoryName = selectedCategory?.name, let itemName = itemsArray[indexPath.row].title {
+                    let request: NSFetchRequest<ItemModel> = ItemModel.fetchRequest()
+                    let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", categoryName)
+                    let itemPredicate = NSPredicate(format: "title MATCHES %@", itemName)
+                    request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, itemPredicate])
+                    if let results = try? context.fetch(request) {
+                        for object in results {
+                            context.delete(object)
+                        }
+                        itemsArray.remove(at: indexPath.row)
+                        saveItems()
+                        tableView.deleteRows(at: [indexPath], with: .automatic)
+                    }
+                }
+            }
+        }
+    }
+        private func getData() {
+            loadItem()
+        }
+        private func saveItems() {
+            do {
+                try context.save()
+            } catch {
+                print("Error save context")
+            }
+        }
+        private func loadItem(with request: NSFetchRequest<ItemModel> = ItemModel.fetchRequest(), predicate: NSPredicate? = nil) {
+            guard let name = selectedCategory?.name else { return }
+            let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", name)
+            if let predicate = predicate {
+                request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate, categoryPredicate])
+            } else {
+                request.predicate = categoryPredicate
+            }
+            do { itemsArray = try context.fetch(request) }
+            catch {
+                print("error")
+            }
+            tableView.reloadData()
+        }
+    }
+    extension ToDoViewController: UISearchBarDelegate {
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            
+            if searchText.isEmpty {
+                loadItem()
+                searchBar.resignFirstResponder()
+            } else {
+                let request: NSFetchRequest<ItemModel> = ItemModel.fetchRequest()
+                let searchPredicate = NSPredicate(format: "title CONTAINS %@", searchText)
+                request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+                loadItem(with: request, predicate: searchPredicate)
+            }
+        }
     }
   
-
-  
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-   
-
-}
