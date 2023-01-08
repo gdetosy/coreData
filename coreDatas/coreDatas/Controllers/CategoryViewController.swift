f121e4a806c07f0901bf5368091a2de6147f14fe
gdetosy <tosyjdm@gmail.com>
2023-01-08 16:12:22 +0300

    add categories

--- coreDatas/coreDatas/Controllers/CategoryViewController.swift
+++ coreDatas/coreDatas/Controllers/CategoryViewController.swift
@@ -6,56 +6,67 @@
 //
 
 import UIKit
-
+import CoreData
 class CategoryViewController: UITableViewController {
-
+    var categories =  [CategoryModel]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
-
+    
     @IBAction func addNewCategory(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        alert.addTextField { textField in textField.placeholder = "Category"
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
+            if let textField = alert.textFields?.first,
+               let text = textField.text,
+               text != "",
+               let self = self
+            {
+                let newCategory = CategoryModel(context: self.context)
+                newCategory.name = text
+                self.categories.append(newCategory)
+                self.saveCategories()
+                self.tableView.insertRows(at: [IndexPath(row: self.categories.count - 1, section: 0)], with: .automatic)
+            }
+        }
+        alert.addAction(cancel)
+        alert.addAction(addAction)
+        
+        self.present(alert, animated: true)
     }
-    
-    
-    
     
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        categories.count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name
         return cell
     }
-    */
-
-    /*
+    
+    
+    // MARK: - Table view delegate
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -63,33 +74,37 @@ class CategoryViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+    //   MARK: - CoreData
 
+    private func saveCategories() {
+        do {
+            try context.save()
+            
+        } catch {
+            print("\(error)")
+        }
+    }
+
+    private func loadCategories(with request: NSFetchRequest<CategoryModel> = CategoryModel.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+            
+        } catch {
+            print("error")
+        }
+        tableView.reloadData()
+    }
+    
 }
